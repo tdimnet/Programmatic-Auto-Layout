f18cc3dd9350cdd2eaebf3440921bfe02ac8f841
tdimnet <thomas.dimnet@gmail.com>
2018-07-10 11:42:06 +0200

    Create building blocks for the auto-layout.

--- ProgrammaticAutoLayout/ViewController.swift
+++ ProgrammaticAutoLayout/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        redView.backgroundColor = .red
         view.addSubview(redView)
     }
     
@@ -26,6 +27,42 @@ class ViewController: UIViewController {
         super.viewWillLayoutSubviews()
         
         let horizontalCenter = NSLayoutConstraint(item: redView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+        let redViewWidthConstraint = NSLayoutConstraint(item: redView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 200)
+        let redViewHeightConstraint = NSLayoutConstraint(item: redView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 100)
+        let redViewBottomSpaceConstraint = NSLayoutConstraint(item: redView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: -50)
+        
+        NSLayoutConstraint.activate([
+            horizontalCenter,
+            redViewWidthConstraint,
+            redViewHeightConstraint,
+            redViewBottomSpaceConstraint
+        ])
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
